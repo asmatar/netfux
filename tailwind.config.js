fd497632b5b66e4aba0b5cc5bbc8b8c8e8b42b53
Arthur <arthur@macbook-arthur.local>
2022-12-12 13:55:02 +0100

    fix

--- tailwind.config.js
+++ tailwind.config.js
@@ -21,7 +21,7 @@ module.exports = {
         'net-gray': '#696969',
       },
       backgroundImage: {
-        'gradient':'linear-gradient(to bottom,rgba(20,20,20,0) 0,rgba(20,20,20,.15) 15%,rgba(20,20,20,.35) 29%,rgba(20,20,20,.58) 44%,#141414 68%,#141414 100%);',
+        'gradient':'linear-gradient(to bottom,rgba(20,20,20,0) 0,rgba(20,20,20,.15) 15%,rgba(20,20,20,.35) 29%,rgba(20,20,20,.58) 44%,#141414 68%,#141414 100%)',
         login: "url(/images/login/netflixbg1.jpg)",
       },
       keyframes: {
