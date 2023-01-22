d28af6728afbf3031f1bbe45943be66bcbd8e811
Arthur <arthur@macbook-arthur.local>
2023-01-22 17:34:44 +0100

    modal clos

--- pages/index.js
+++ pages/index.js
@@ -37,7 +37,7 @@ export default function Home({favMovies}) {
       </Head>
       <main>
         <Banner />
-        <section className="flex flex-col gap-y-8 pl-4 big-phone:pl-6 lg:pl-12 mt-10">
+        <section className="flex flex-col gap-y-8 pl-4 big-phone:pl-6 lg:pl-12 mt-10 md:mt-0">
         <ToastContainer />
           { filteredMovie.length <1 && activeSearch === false ? (
           <>
