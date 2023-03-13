852787a4d6dccca4977502ff53552bf4299054a3
Arthur <arthur@macbook-arthur.local>
2023-03-13 19:32:39 +0100

    last clean

--- components/Filmcard.js
+++ components/Filmcard.js
@@ -16,7 +16,28 @@ const FilmCard = ({film}) => {
   }
 
   return (
-    <div className={`relative flex-col cursor-pointer h-28 min-w-[180px] md:h-36 md:min-w-[260px]`} onClick={()=>handleModal(film.id, type)}å>
+    <div className={`relative flex flex-col cursor-pointer transition duration-200 ease-out md:hover:scale-105 h-28 min-w-[180px] md:h-36 md:min-w-[260px]`} onClick={()=>handleModal(film.id, type)}>
+    <Image
+      data-imagefilmid={film.id}
+      src={`https://image.tmdb.org/t/p/w500${
+        film?.backdrop_path || film?.poster_path
+      }`}
+      className="rounded-sm object-cover md:rounded"
+      fill
+      alt="film card image"
+    />
+    <div className="absolute flex flex-col items-start justify-end gap-y-2 h-full bottom-0 left-0 w-full bg-black/40 text-xs hover:opacity-100 hover:visible visible opacity-0 transition duration-200 ease">
+      <h6 className="font-NetflixBold self-start">{film?.title || film?.name}</h6>
+      <p className="">{film?.overview?.substring(0, 100) + "..."}</p> 
+    </div>
+  </div> 
+  )
+}
+
+export default FilmCard
+
+/* scale version not for production */
+    {/* <div className={`relative flex-col cursor-pointer h-28 min-w-[180px] md:h-36 md:min-w-[260px] hover:min-h-full`} onClick={()=>handleModal(film.id, type)}å>
       <div class="transition duration 200 h-full group">
         <Image
           data-imagefilmid={film.id}
@@ -33,26 +54,4 @@ const FilmCard = ({film}) => {
           <p className="">{film?.overview?.substring(0, 100) + "..."}</p> 
         </div>
       </div>
-    </div>
-  )
-}
-
-export default FilmCard
-{/* <> */}
-  {/* <div className={`relative flex flex-col cursor-pointer transition duration-200 ease-out md:hover:scale-105 h-28 min-w-[180px] md:h-36 md:min-w-[260px]`} onClick={()=>handleModal(film.id, type)}>
-    <Image
-      data-imagefilmid={film.id}
-      src={`https://image.tmdb.org/t/p/w500${
-        film?.backdrop_path || film?.poster_path
-      }`}
-      className="rounded-sm object-cover md:rounded"
-      fill
-      alt="film card image"
-    />
-    <div className="absolute flex flex-col items-start justify-end gap-y-2 h-full bottom-0 left-0 w-full bg-black/40 text-xs hover:opacity-100 hover:visible visible opacity-0 transition duration-200 ease">
-      <h6 className="font-NetflixBold self-start">{film?.title || film?.name}</h6>
-      <p className="">{film?.overview?.substring(0, 100) + "..."}</p> 
-    </div>
-  </div>  */}
-  
-{/*   </> */}
+    </div> */}
